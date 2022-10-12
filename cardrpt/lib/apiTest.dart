1ada9884d95b31e174630b3f2350f5b0b47d8182
Kim-Tae-Kwan <vodjaos@gmail.com>
2022-10-12 18:05:39 +0900

    cardrpt 제거.

--- cardrpt/lib/apiTest.dart
+++
@@ -1,8 +0,0 @@
-import 'package:cardrpt/departmentService.dart';
-import 'package:cardrpt/models/department.dart';
-
-void main() async{
-  DepartmentService service = DepartmentService();
-  ByDept byDept = await service.getReport();
-  print(byDept.rec![0].toJson());
-}
