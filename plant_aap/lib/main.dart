153d1b7a023db031f446446a5e0415f0e75974d0
Kim-Tae-Kwan <vodjaos@gmail.com>
2022-10-19 16:15:50 +0900

    plant app firebase storage 추가.

--- plant_aap/lib/main.dart
+++ plant_aap/lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:plant_aap/constants.dart';
 import 'package:plant_aap/screens/home/home_screen.dart';
+import 'package:plant_aap/firebase_options.dart';
 
-void main() {
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
